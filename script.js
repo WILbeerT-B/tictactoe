d9e8c96e262192643a64fc4702d7e2c03111b210
wb <wilbertbundalian@gmail.com>
2025-04-04 06:33:20 -0700

    Update script file to add functionality
    Add displayBoard function to easily call
    and display the board, add players array.

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let count = 1;
 
-console.log(`${board[0]} | ${board[1]} | ${board[2]}`);
-console.log(`${board[3]} | ${board[4]} | ${board[5]}`);
-console.log(`${board[6]} | ${board[7]} | ${board[8]}`);
+//function to display the board
+function displayBoard() {
+    console.log(`${board[0]} | ${board[1]} | ${board[2]}`);
+    console.log(`${board[3]} | ${board[4]} | ${board[5]}`);
+    console.log(`${board[6]} | ${board[7]} | ${board[8]}`);
+    console.log('');
+}
+
+const players = [
+    {
+        name: "Player One",
+        marker: "X"
+    },
+    {
+        name: "Player Two",
+        marker: "O"
+    }
+]
+
+displayBoard();
 
 while (count <= 9) {
     let input = prompt('Player, please choose a cell');
+    board[input - 1] = "X";
+    displayBoard();
     count++;
 }
