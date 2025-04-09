e6b9f74cad79f33ad5c45842b205d4dcb7feb116
wb <wilbertbundalian@gmail.com>
2025-04-08 23:01:49 -0700

    Update script file
    Add win combo array to store winning
    combinations, add checkWinner function.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let count = 1;
 
+WIN_COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
+
 //function to display the board
 function displayBoard() {
     console.log(`${board[0]} | ${board[1]} | ${board[2]}`);
@@ -31,7 +33,27 @@ while (count <= 9) {
         activePlayer = players[0];
     }
     let input = prompt(`${activePlayer.name}, your marker is ${activePlayer.marker}. \nPlease choose a cell from 1-9`);
+    console.log(`${activePlayer.name} put an ${activePlayer.marker} in cell ${board[input - 1]}`);
     board[input - 1] = activePlayer.marker;
     displayBoard();
+    checkWinner();
     count++;
+}
+
+function checkWinner() {
+    let pos1, pos2, pos3;
+    WIN_COMBOS.forEach(element => {
+        // console.log(element);
+        pos1 = board[element[0]];
+        pos2 = board[element[1]];
+        pos3 = board[element[2]];
+        if (pos1 == pos2 && pos2 == pos3) {
+            count = 10;
+            if (pos1 == "X") {
+                console.log("Player 1 wins");
+            } else {
+                console.log("Player 2 wins");
+            }
+        }
+    });
 }
